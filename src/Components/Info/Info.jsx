9893c2d324087dec8104ddac4b435c1eca19ae41
Garvit Agrawal <agarvit74@gmail.com>
2023-07-10 14:56:29 +0530

    Completed Info Section

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { BsBookmarkCheck, BsShieldCheck } from 'react-icons/bs'
+import { RxCalendar } from 'react-icons/rx'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className='info section'>
+      <div className="infoContainer container">
+        <div className="titlediv flex">
+          <h2>Travel to make memories all around the world</h2>
+          <button className='btn'>
+            View All
+          </button>
+          <div className="cardsDiv grid">
+            <div className="singleCard grid">
+              <div className="iconDiv flex">
+                <RxCalendar className='icon'/>
+              </div>
+              <span className="cardTitle">
+                Book & Relax
+              </span>
+              <p>You can also call airlines from your phone and book a flight ticket</p>
+            </div>
+            <div className="singleCard grid">
+              <div className="iconDiv flex">
+                <BsShieldCheck className='icon'/>
+              </div>
+              <span className="cardTitle">
+                Smart Checklist
+              </span>
+              <p>You can also call airlines from your phone and book a flight ticket</p>
+            </div>
+            <div className="singleCard grid">
+              <div className="iconDiv flex">
+                <BsBookmarkCheck className='icon'/>
+              </div>
+              <span className="cardTitle">
+                Save More
+              </span>
+              <p>You can also call airlines from your phone and book a flight ticket</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
